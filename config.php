62c7db9b28628dca369294a22a66d6a34f8db37e
elenaticau <56132225+elenaticau@users.noreply.github.com>
2019-10-16 00:49:02 +0300

    Add files via upload

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+defined('VG_ACCESS') or die ('Access denied');
+const Site_URL = 'http://in.my';
+const PATH = '/';
+
+const HOST = 'localhoost';
+const USER = 'root';
+const PASS = '';
+const DB_NAME = 'Magazin';
---
+++ internal_settings.php
@@ -0,0 +1,22 @@
+<?php
+defined('VG_ACCESS') or die ('Access denied');
+const TEMPLATE = 'templates/default/';
+const ADMIN_TEMLATES = 'core/admin/views/';
+
+const COOKIE_VERSION = '1.0.0';
+const CRIPT_KEY = '';
+const COOKIE_TIME = 60;
+const BLOCK_TIME = 3;
+
+const QTY = 8;
+const QTY_LINES = 3;
+
+const ADMIN_CSS_JS = [
+    'styles' => [],
+    'scripts' => []
+];
+
+const USER_CSS_JS = [
+    'styles' => [],
+    'scripts' => []
+];
